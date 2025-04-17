f9d8d53cc6bb35e984c46fe8d8ba9de6b76ec64b
Aurelien <ademaille@gmail.fr>
2025-04-17 11:04:29 +0200

    Mise en commentaire de "l'ancien" code js pour mettre le nouveau

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 // Script comme euromillions mais maintenant à modifier en orienté objet
 
-function genererNombres(nbNombres, nbMax) {
-    const uniqueNumbers = [];
-    while (uniqueNumbers.length < nbNombres) {
-        let newNumber = Math.ceil(Math.random() * nbMax);
-        if (uniqueNumbers.indexOf(newNumber) == -1) {
-            uniqueNumbers.push(newNumber);
-        }
-    }
-    console.log(uniqueNumbers);
-    return uniqueNumbers;
-}
 
 
-const button = document.querySelector(".tirage");
-button.addEventListener("click", () => {
+// function genererNombres(nbNombres, nbMax) {
+//     const uniqueNumbers = [];
+//     while (uniqueNumbers.length < nbNombres) {
+//         let newNumber = Math.ceil(Math.random() * nbMax);
+//         if (uniqueNumbers.indexOf(newNumber) == -1) {
+//             uniqueNumbers.push(newNumber);
+//         }
+//     }
+//     console.log(uniqueNumbers);
+//     return uniqueNumbers;
+// }
 
-    const nombresStandard = genererNombres(5, 50);
 
-    const nombresEtoiles = genererNombres(2, 12);
+// const button = document.querySelector(".tirage");
+// button.addEventListener("click", () => {
 
-    const numeros = document.querySelectorAll(".rond-text");
+//     const nombresStandard = genererNombres(5, 50);
 
-    for (let i = 0; i < numeros.length; i++) {
-        numeros[i].textContent = nombresStandard[i];
-        console.dir(numeros[i])
-    }
+//     const nombresEtoiles = genererNombres(2, 12);
 
-    const numerosTwo = document.querySelectorAll(".étoile-text");
+//     const numeros = document.querySelectorAll(".rond-text");
 
-    for (let i = 0; i < numerosTwo.length; i++) {
-        numerosTwo[i].textContent = nombresEtoiles[i];
-        console.dir(numerosTwo[i])  
-    }           
-});
+//     for (let i = 0; i < numeros.length; i++) {
+//         numeros[i].textContent = nombresStandard[i];
+//         console.dir(numeros[i])
+//     }
+
+//     const numerosTwo = document.querySelectorAll(".étoile-text");
+
+//     for (let i = 0; i < numerosTwo.length; i++) {
+//         numerosTwo[i].textContent = nombresEtoiles[i];
+//         console.dir(numerosTwo[i])  
+//     }           
+// });
